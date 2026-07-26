9535eaf544c32b21c50fc41b6e9442fcba832979
Param <param@Params-MacBook-Air.local>
2026-07-27 02:17:34 +0530

    engine: add logging and state_manager connect

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,27 +1,35 @@
 #include "../include/engine.h"
+#include "common/include/logger.h"
 #include <iostream>
 
 Engine::Engine() {
+    LOG("Engine", "Constructed");
     algo_manager_.setFeedManager(&feed_manager_);
+    state_manager_.connect();
 }
 
 Engine::~Engine() {
+    LOG("Engine", "Destroying");
     stop();
+    LOG("Engine", "Destroyed");
 }
 
 void Engine::start() {
+    LOG("Engine", "start() called");
     running_ = true;
     feed_manager_.start_feed();
-    std::cout << "Engine: Started" << std::endl;
+    LOG("Engine", "Started");
 }
 
 void Engine::stop() {
+    LOG("Engine", "stop() called");
     running_ = false;
     feed_manager_.stop_feed();
-    std::cout << "Engine: Stopped" << std::endl;
+    LOG("Engine", "Stopped");
 }
 
 void Engine::restart() {
+    LOG("Engine", "restart() called");
     stop();
     start();
 }
