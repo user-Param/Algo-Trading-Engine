30cf2354b48fb50e1951088e19a47d052f62b63b
Param <param@Params-MacBook-Air.local>
2026-07-27 02:17:38 +0530

    feed: add SSL WS support, callbacks, observers

--- feed/include/feed_manager.h
+++ feed/include/feed_manager.h
@@ -5,13 +5,21 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <functional>
+#include <thread>
+#include <unordered_map>
+#include "../../common/include/logger.h"
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
+#include <boost/beast/ssl.hpp>
 #include <boost/asio/strand.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 namespace net = boost::asio;
+namespace ssl = boost::asio::ssl;
 using tcp = boost::asio::ip::tcp;
 
 struct MarketData {
@@ -22,6 +30,8 @@ struct MarketData {
     uint64_t volume = 0;
     uint64_t timestamp = 0;
 };
+
+using MarketDataCallback = std::function<void(const MarketData&)>;
 
 class FeedManager {
 public:
@@ -42,10 +52,24 @@ public:
     void subscribe_topics(const std::vector<std::string>& symbols);
     MarketData get_feed(const std::string& symbol);
 
+    void set_market_data_callback(MarketDataCallback cb);
+    void register_observer(MarketDataCallback cb);
+
 private:
+    void on_read(beast::error_code ec, std::size_t bytes_transferred);
+    void handle_message(const std::string& msg);
+    void do_read();
+
     bool backtest_mode_ = false;
     bool connected_ = false;
-    std::unique_ptr<websocket::stream<beast::tcp_stream>> ws_;
-    std::vector<std::string> subscribed_symbols_;
+    MarketDataCallback callback_;
+    std::vector<MarketDataCallback> observers_;
     net::io_context ioc_;
+    ssl::context ctx_{ssl::context::tlsv12_client};
+    std::unique_ptr<websocket::stream<beast::ssl_stream<beast::tcp_stream>>> ws_;
+    std::unique_ptr<tcp::resolver> resolver_;
+    beast::flat_buffer buffer_;
+    std::vector<std::string> subscribed_symbols_;
+    std::thread io_thread_;
+    std::unordered_map<std::string, MarketData> latest_data_;
 };
