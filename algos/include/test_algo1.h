8144be1afe5d61f72343ce7fdebe0fadeae5655e
Param <param@Params-MacBook-Air.local>
2026-08-02 06:55:49 +0530

    test_algo1: add TestAlgo1 class header extending BaseAlgo

---
+++ algos/include/test_algo1.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "base_algo.h"
+#include <memory>
+#include <string>
+
+class TestAlgo1 : public BaseAlgo {
+public:
+    explicit TestAlgo1(const std::string& id);
+    void onMarketData(const std::string& symbol, const MarketData& data) override;
+    Signal generateSignal() override;
+    std::string getId() const override;
+    void start() override;
+    void stop() override;
+
+private:
+    std::string id_;
+    bool running_ = false;
+};
+
+std::unique_ptr<BaseAlgo> createTestAlgo1();
